4049a168e165d8328880c6d08912fb6d64bf47f5
sideeqa <sideeqahabdulkareem@gmail.com>
2025-12-10 14:13:33 +0100

    new

---
+++ java-snacks/nokia3310.java
@@ -0,0 +1,110 @@
+import java.util.scanner
+public class Nokia- 3310{
+     public class void main(string...args){
+    
+        Scanner input =new scanner(system.in)
+        string menuFunctions ="""
+
+        PHONE MENU
+        
+
+        Phone Book
+
+        Messages
+        Chat
+        Call register
+        Tones 
+        Settings
+        Call divert
+        Games
+        Calculator
+        Remainder
+        Clock
+        Profile
+        Sim services
+""";
+system.put.println(menuFuntion);
+int menufunctionOption=input.nextInt();
+switch (menuFunction){
+case 1 -> {system.out.println("phonebook");
+      
+
+
+    String phoneBook= """
+
+    Search
+    Service nos.
+    Add name
+    Erase
+    Edit
+    Assign tone
+    Send b card
+    Option
+    Speed dial
+    Voice tags
+""";
+    Syste.out.printlin(phonebook);
+    int phonebookOptions = input.nextInt()();
+    Switch ( phoneBookOptions){
+    case 1-> System.out.println("search"); 
+    case 2-> System.out.printl("Service nos"); 
+    case 3-> System.out.println("Add names"); 
+    case 4-> System.out.println("Erase"); 
+    case 5-> System.out.println("Edit"); 
+    case 6-> System.out.println("Assign"); 
+    case 7-> System.out.println("Send b board"); 
+    case 8-> System.out.println("Option");
+    
+    string option = """
+            Type of view
+            memory status
+
+      """;
+
+        System.out.print(options){
+        int optionoption = input.nextInt();
+
+        switch (optionsOption){
+
+        case 1 -> System.out.println("Type of view");
+        case 2 -> System.out.println(" Memory Status");
+        default-> System.out.println("Invalid input");
+                    {
+
+
+                }
+     //END OF CASE 1
+case 2-> System.out.println("Messages");
+case 3-> System.out.println(" Chat");
+case 4-> System.out.println("Call register");
+case 5-> System.out.println("Tones");
+case 6-> System.out.println("setting");
+case 7-> System.out.println("Call diverts");
+case 8-> System.out.println("Games");
+case 9-> System.out.println("Calculator");
+case 10-> System.out.println("Remainder");
+case 11> System.out.println("Clock");
+case 12-> System.out.println("Profiles");
+case 13-> System.out.println("Sim service");
+default-> System.out.println("Invalid input");
+
+}
+
+
+
+}
+
+}
+int messagemenu= obtain.nextint();
+
+
+
+
+
+
+
+
+
+
+
+}
